8b97b9bb92a0c38eb86ad799f2d5eb6d0a5b6b35
Liry <learainccc@outlook.com>
2025-05-22 21:41:56 +0800

    增加自动部署

--- packages/blog-rspress/rspress.config.ts
+++ packages/blog-rspress/rspress.config.ts
@@ -7,6 +7,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const rspressConfig = defineConfig({
   root: join(__dirname, 'docs'),
   title: 'Liry Blog',
+  base: '/blog',
   themeConfig: {
     socialLinks: [
       {
