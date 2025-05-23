d432dd6a391cd1e102b39de88472c00d06792593
Liry <learainccc@outlook.com>
2025-05-23 08:32:33 +0800

    去除assets包

--- packages/assets/src/contacts/index.ts
+++
@@ -1,5 +0,0 @@
-import LIRY_LARK_QRCODE from './lark-qrcode.jpg';
-import LIRY_QQ_QRCODE from './qq-qrcode.jpg';
-import LIRY_WECHAT_QRCODE from './wechat-qrcode.jpg';
-
-export { LIRY_LARK_QRCODE, LIRY_QQ_QRCODE, LIRY_WECHAT_QRCODE };
