dcb42fdf554a0dac27a45c23c0722a60112b6aa9
Liry <learainccc@outlook.com>
2025-07-13 17:04:38 +0800

    增加cspell检查错误

---
+++ cspell.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'cspell';
+import { readFileSync } from 'node:fs';
+
+const cspellConfig = defineConfig({
+  version: '0.2',
+  ignorePaths: [
+    ...readFileSync('.gitignore')
+      .toString()
+      .split('\n')
+      .filter((v) => {
+        return !v.startsWith('#');
+      }),
+    ...readFileSync('.prettierignore')
+      .toString()
+      .split('\n')
+      .filter((v) => {
+        return !v.startsWith('#');
+      })
+  ]
+});
+
+export default cspellConfig;
